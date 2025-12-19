5e0b021a0f364873c549670abafeaafde242482b
Zoront22 <osvaldoNajeraSandoval@outlook.com>
2025-12-19 12:56:27 -0600

    bucles

---
+++ Bucles/For/BucleFor.java
@@ -0,0 +1,13 @@
+public class BucleFor {
+    public static void main(String[] args) {
+        // Bucle for que imprime los números del 1 al 10
+
+        // agregamos una variable de control 'i' que inicia en 1
+        // la condición del bucle es que 'i' sea menor o igual a 10
+        // y en cada iteración incrementamos 'i' en 1
+        for (int i = 1; i <= 10; i++) {
+            // imprimimos el valor actual de 'i' en cada iteración
+            System.out.println(i);
+        }
+    }
+}
